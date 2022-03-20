d0eb99347d4be11cd077dbd840ff5f49a02412da
Armon Ahmadi <abstrack@Armons-MacBook-Air.local>
2022-03-20 04:32:14 -0400

    added dead or alive function

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,13 +17,24 @@ var fight = function() {
   console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
+  //checks enemy's health
+  if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+  }
+  else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left!");
+  }
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
   // Log a resulting message to the console so we know that it worked.
-  console.log (
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-  )
-};
+  console.log (enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+  //check players health
+  if (playerHealth <= 0)
+    window.alert(playerName + " has died!");
+  else {
+      window.alert(playerName + " still has " + playerHealth + " health remaining!");
+  }
+}
 
 
 
