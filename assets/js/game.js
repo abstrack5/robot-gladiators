f1ea339ed4b20fe649d7395ab7310c72003ce0c9
Abstrack <Armon_ah05@outlook.com>
2022-03-24 17:03:08 -0400

    fight now works

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,44 +1,42 @@
-/* GAME FUNCTIONS */
-
 // function to generate a random numeric value
 var randomNumber = function(min, max) {
-  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  var value = Math.floor(Math.random() * (max - min) + min);
 
   return value;
 };
 
-
+// function to check if player wants to fight or skip
 var fightOrSkip = function() {
-  // ask player if they'd like to fight or skip using fightOrSkip function
+  // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
+  // validate prompt answer
   if (promptFight === "" || promptFight === null) {
     window.alert("You need to provide a valid answer! Please try again.");
+    // use return to call it again and stop the rest of this function from running
     return fightOrSkip();
   }
 
+  // convert promptFight to all lowercase so we can check with less options
   promptFight = promptFight.toLowerCase();
 
-  // if player picks "skip" confirm and then stop the loop
-  if (promptFight === "skip" || promptFight === "SKIP") {
+  if (promptFight === "skip") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
     // if yes (true), leave fight
     if (confirmSkip) {
       window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerInfo.playerMoney = playerInfo.money - 10;
+      // subtract money from playerMoney for skipping, but don't let them go into the negative
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+      // stop while() loop using break; and enter next fight
 
-      return true();
-
-      shop();
+      // return true if player wants to leave
+      return true;
     }
   }
-
-  return false();
-
-}
+  return false;
+};
 
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
