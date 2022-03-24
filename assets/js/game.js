ce6b5c689424cafeffbccb12ceb11778e53c9d68
Abstrack <Armon_ah05@outlook.com>
2022-03-24 12:58:33 -0400

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,6 +80,15 @@ var startGame = function () {
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
+
+    // if player is still alive we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length -1); {
+        var storeConfirm = window.confirm("The fight is over, visit the store before next round?");
+    
+        if(storeConfirm) {
+          shop();
+        }
+      }
     }else {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
@@ -110,7 +119,46 @@ var endGame = function() {
 }
 
 var shop = function() {
-  console.log("entered the shop");
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+
+    switch (shopOptionPrompt) {
+      case "REFILL": // new case
+      case "refill":
+        if (playerMoney >= 7) {
+          window.alert("Refilling player's health by 20 for 7 dollars.");
+    
+          playerHealth = playerHealth + 20;
+          playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+    
+        break;
+      case "UPGRADE": // new case
+      case "upgrade":
+        if (playerMoney >= 7) {
+          window.alert("Upgrading player's attack by 6 for 7 dollars.");
+    
+          playerAttack = playerAttack + 6;
+          playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+    
+        break;
+      case "LEAVE": // new case
+      case "leave":
+        window.alert("Leaving the store.");
+        break;
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+        shop();
+        break;
+    }
 }
 
 // start the game when the page loads
